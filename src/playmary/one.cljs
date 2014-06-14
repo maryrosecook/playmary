46dc82dece761b3106f40c231ac3fb36d69e18c6
Mary Rose Cook <maryrosecook@maryrosecook.com>
2014-06-14 13:55:52 -0400

    Make scrolling less sensitive

--- src/playmary/one.cljs
+++ src/playmary/one.cljs
@@ -235,7 +235,7 @@
          (or (= (prev-t :type) "touchmove")
              (and (= (prev-t :type) "touchstart")
                   (= (prev-t :age) 0)
-                  (> (distance (cur-t :position) (prev-t :position)) 5))))))
+                  (> (distance (cur-t :position) (prev-t :position)) 10))))))
 
 (defmulti handle-touch (fn [t touches out-c] (:type t)))
 
